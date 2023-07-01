436490e371138d1c9a97069aee05d44f1f33d699
Xander Masotto <xmasotto@gmail.com>
2023-06-30 17:04:11 -0700

    scripts

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./test_ace.py datasets/7scenes_chess output/7scenes_chess.pt --render_visualization=true
---
+++ train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./train_ace.py datasets/7scenes_chess output/7scenes_chess.pt --training_buffer_size=64000 --epochs=2048
